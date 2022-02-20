4182620d5f3fe11615677087669cb139b9b7f613
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-20 18:38:43 -0500

    added viewFuncs

--- db/schema.sql
+++ db/schema.sql
@@ -8,14 +8,14 @@ DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary FLOAT NOT NULL,
     department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,4 +24,5 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT DEFAULT NULL,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,41 @@
+-- DEPARTMENT SEEDS --
 INSERT INTO department (name)
-VALUES 
-('Information Systems and Technology'),
-('Finance'),
-('Legal'),
-('Human Resources'),
-('Security'),
-('Sales');
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
 
+-- EMPLOYEE ROLE SEEDS --
 INSERT INTO role (title, salary, department_id)
-VALUES
-('Web Developer', 90000, 1),
-('Accountant', 70000, 2),
-('Paralegal', 50000, 3),
-('Manager', 70000, 4),
-('Engineer', 90000, 5),
-('Sales Rep', 40000, 6);
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
 
+-- EMPLOYEE SEEDS --
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES 
-('John', 'Smith', 1, 458),
-('Ronald', 'Young', 2, 276),
-('David', 'Miller', 3, 486),
-('Maria', 'Hall', 4, 126),
-('Linda', 'Martin', 5, 724),
-('Taylor', 'Wilson', 6, 157);
+VALUE ("Jessica", "Haze", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tiffany", "Patric", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Mia","Lam", 3 , null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Bently", "Lao", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Chris", "Melby", 5, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jason", "Baker", 6, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tom", "Smith", 7, 2);
