9936dfbf25a79a0b7220bafbaff65509804637b9
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-20 16:25:57 -0500

    inquirer setup

--- db/schema.sql
+++ db/schema.sql
@@ -1,59 +1,27 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS voters;
 
--- department
-
--- id: INT PRIMARY KEY
-
--- name: VARCHAR(30) to hold department name
-CREATE TABLE parties (
+CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
+    name VARCHAR(30) NOT NULL,
 );
--- role
-
--- id: INT PRIMARY KEY
-
--- title: VARCHAR(30) to hold role title
-
--- salary: DECIMAL to hold role salary
-
--- department_id: INT to hold reference to department role belongs to
-CREATE TABLE candidates (
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary FLOAT NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
+);
+CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    party_id INT,
-    industry_connected BOOLEAN NOT NULL,
-    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
--- employee
-
--- id: INT PRIMARY KEY
-
--- first_name: VARCHAR(30) to hold employee first name
-
--- last_name: VARCHAR(30) to hold employee last name
-
--- role_id: INT to hold reference to employee role
-
--- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
-CREATE TABLE voters (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-CREATE TABLE votes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    voter_id INT NOT NULL,
-    candidate_id INT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT uc_voter UNIQUE (voter_id),
-    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
 );
--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 const db = require('./db/connection');
-const inquirer = requier('inquirer');
-const cTable = require('console.table');
+const inquirer = require('inquirer');
+// response handling 
+const { viewDepartments, viewRoles, viewEmployees, viewByManager, viewByDepartment } = require('./lib/viewFuncs');
+const { addDepartment, addRole, addEmployee } = require('./lib/addFuncs');
+const { updateRole, updateManager } = require('./lib/updateFuncs');
+const { deleteDepartment, deleteRole, deleteEmployee } = require('./lib/deleteFuncs');
 
+function prompt() {
+    inquirer.prompt({
+        name: 'choice',
+        tye: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments', 
+            'View all roles', 
+            'View all employees',
+            'View employees by manager',
+            'View employees by department', 
+            'Add a department', 
+            'Add a role',
+            'Add an employee', 
+            'Update an employee role',
+            'Update employee manager',
+            'Delete a department',
+            'Delete a role',
+            'Delete an employee',
+            'EXIT'
+        ]
+    }).then(answer => {
+        switch (answer.choice) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View employees by manager':
+                viewByManager();
+                break;
+            case 'View employees by department':
+                viewByDepartment();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateRole();
+                break;
+            case 'Update an employee manager':
+                updateManager();
+                break;
+            case 'Delete a department':
+                deleteDepartment();
+                break;
+            case 'Delete a role':
+                deleteRole();
+                break;
+            case 'Delete an employee':
+                deleteEmployee();
+                break;
+            case 'EXIT': 
+                exitApp();
+                break;
+            default:
+                break;
+        }
+    });
+};
+// connect to database 
 db.connect(err => {
     if (err) throw err;
     console.log('Welcome to the employee-tracker!');
 });
-
-// You might also want to make your queries asynchronous. 
-// MySQL2 exposes a .promise() function on Connections to upgrade an existing non-Promise connection to use Promises. 
-// To learn more and make your queries asynchronous, 
-// refer to the npm documentation on MySQL2.
+// exit the app
+function exitApp() {
+    db.end();
+};
+// start the app 
+prompt();
