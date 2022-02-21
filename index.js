eca7f47aabf9fbc09cb33a0cbbf182e147afde9f
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-21 03:35:07 -0500

    refactored index.js

--- index.js
+++ index.js
@@ -7,90 +7,35 @@ const { addDepartment, addRole, addEmployee } = require('./lib/addFuncs');
 const { updateRole, updateManager } = require('./lib/updateFuncs');
 const { deleteDepartment, deleteRole, deleteEmployee } = require('./lib/deleteFuncs');
 
-function prompt() {
-    inquirer.prompt({
+const choicesObj = {
+    'View all departments': viewDepartments, 
+    'View all roles': viewRoles, 
+    'View all employees': viewEmployees,
+    'View employees by manager': viewByManager,
+    'View employees by department': viewByDepartment, 
+    'Add a department': addDepartment, 
+    'Add a role': addRole,
+    'Add an employee': addEmployee, 
+    'Update an employee role': updateRole,
+    'Update employee manager': updateManager,
+    'Delete a department': deleteDepartment,
+    'Delete a role': deleteRole,
+    'Delete an employee': deleteEmployee,
+    'EXIT': exitApp
+}
+const prompt = async () => {
+    const answer = await inquirer.prompt({
         name: 'choice',
         type: 'list',
         message: 'What would you like to do?',
-        choices: [
-            'View all departments', 
-            'View all roles', 
-            'View all employees',
-            'View employees by manager',
-            'View employees by department', 
-            'Add a department', 
-            'Add a role',
-            'Add an employee', 
-            'Update an employee role',
-            'Update employee manager',
-            'Delete a department',
-            'Delete a role',
-            'Delete an employee',
-            'EXIT'
-        ]
-    }).then(answer => {
-        switch (answer.choice) {
-            case 'View all departments':
-                new Promise(function (res) {
-                    viewDepartments()
-                    res(console.log('\n'), prompt())
-                })
-                break;
-            case 'View all roles':
-                new Promise(function (res) {
-                    viewRoles()
-                    res(prompt())
-                })
-                break;
-            case 'View all employees':
-                new Promise(function (res) {
-                    viewEmployees()
-                    res(prompt())
-                })
-                break;
-            case 'View employees by manager':
-                new Promise(function (res) {
-                    viewByManager()
-                    res(prompt());
-                })
-                break;
-            case 'View employees by department':
-                new Promise(function (res) {
-                    viewByDepartment()
-                    res(prompt());
-                })
-                break;
-            case 'Add a department':
-                addDepartment();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                updateRole();
-                break;
-            case 'Update an employee manager':
-                updateManager();
-                break;
-            case 'Delete a department':
-                deleteDepartment();
-                break;
-            case 'Delete a role':
-                deleteRole();
-                break;
-            case 'Delete an employee':
-                deleteEmployee();
-                break;
-            case 'EXIT': 
-                exitApp();
-                break;
-            default:
-                break;
-        }
+        choices: Object.keys(choicesObj)
     })
+    if (answer) {
+        choicesObj[answer.choice](); 
+        setTimeout(function(){
+            prompt(); // recursion after 2s
+        }, 2000);
+    }
 };
 // connect to database 
 db.connect(err => {
