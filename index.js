346f5933fc73276007b6f035253084601b859a03
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-21 21:11:21 -0500

    fully functional with callback

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-const db = require('./db/connection');
 const inquirer = require('inquirer');
+const db = require('./db/connection');
+const prompt = require('./lib/prompt')
 
-// response handling 
-const { viewDepartments, viewRoles, viewEmployees, viewByManager, viewByDepartment } = require('./lib/viewFuncs');
-const { addDepartment, addRole, addEmployee } = require('./lib/addFuncs');
-const { updateRole, updateManager } = require('./lib/updateFuncs');
-const { deleteDepartment, deleteRole, deleteEmployee } = require('./lib/deleteFuncs');
-
-const choicesObj = {
-    'View all departments': viewDepartments, 
-    'View all roles': viewRoles, 
-    'View all employees': viewEmployees,
-    'View employees by manager': viewByManager,
-    'View employees by department': viewByDepartment, 
-    'Add a department': addDepartment, 
-    'Add a role': addRole,
-    'Add an employee': addEmployee, 
-    'Update an employee role': updateRole,
-    'Update employee manager': updateManager,
-    'Delete a department': deleteDepartment,
-    'Delete a role': deleteRole,
-    'Delete an employee': deleteEmployee,
-    'EXIT': exitApp
-}
-function prompt() {
-    inquirer.prompt([
-        {
-            name: 'choice',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: Object.keys(choicesObj)
-        }
-    ]).then(answer => {
-        new Promise(function (res) {
-            choicesObj[answer.choice]()
-            // res(prompt())
-        });
-    });
-};
-
-// const prompt = async () => {
-//     const answer = await inquirer.prompt({
-//         name: 'choice',
-//         type: 'list',
-//         message: 'What would you like to do?',
-//         choices: Object.keys(choicesObj)
-//     })
-//     if (answer) {
-//         choicesObj[answer.choice](); 
-//         setTimeout(function(){
-//             prompt(); // recursion after 2s
-//         }, 2000);
-//     }
-// };
 // connect to database 
 db.connect(err => {
     if (err) throw err;
@@ -60,8 +9,3 @@ db.connect(err => {
     // start the app 
     prompt();
 });
-// exit the app
-function exitApp() {
-    console.log('Thank you for using the employee-tracker!')
-    db.end();
-};
